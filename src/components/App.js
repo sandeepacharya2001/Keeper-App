1205a82cbe922a3ebd3800b52928ea450094599f
Sandeep Acharya <075bct074.sandeep@pcampus.edu.np>
2021-05-11 09:24:19 +0545

    React Keeper App Basic

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note"
+import notes from "../notes";
+
+function App(){
+    return (
+        <div>
+            <Heading />
+            {notes.map(note =>
+                
+                <Note 
+                key={note.key}
+                title={note.title}
+                content={note.content}
+                />
+            )}
+            <Footer />
+        </div>
+    );
+}
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+const currentYear = new Date().getFullYear();
+function Footer(){
+    return (
+        <footer>
+            <p>Copyright &copy; {currentYear}</p>
+        </footer>
+    );
+}
+
+export default Footer;
