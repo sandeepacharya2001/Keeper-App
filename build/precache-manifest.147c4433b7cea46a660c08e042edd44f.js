367a3630fe05676c5ca7378f8b0e2035e215d16d
Sandeep Acharya <075bct074.sandeep@pcampus.edu.np>
2021-05-11 09:38:25 +0545

    Added build folder

---
+++ build/precache-manifest.147c4433b7cea46a660c08e042edd44f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba831feded4449b194bb320ebdd13f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "f968a134456077eac044",
+    "url": "/static/js/main.0d83627a.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
